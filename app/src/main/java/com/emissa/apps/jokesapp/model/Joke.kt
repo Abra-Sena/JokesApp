a84522a49e77ba5dba336d55a1c16422173018d5
Emissa <temiline@gmail.com>
2022-03-30 02:29:23 -0400

    Feat: create jokes database and dao interface

--- app/src/main/java/com/emissa/apps/jokesapp/model/Joke.kt
+++ app/src/main/java/com/emissa/apps/jokesapp/model/Joke.kt
@@ -1,11 +1,15 @@
 package com.emissa.apps.jokesapp.model
 
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity
 data class Joke(
     @SerializedName("categories")
     val categories: List<String>,
+    @PrimaryKey
     @SerializedName("id")
     val id: Int,
     @SerializedName("joke")
